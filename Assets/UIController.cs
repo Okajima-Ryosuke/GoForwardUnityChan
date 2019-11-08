f3e8740d88d5d468fb0341a2a6142ec9fa395d6f
岡島諒介 <>
2019-11-09 00:03:25 +0900

    no message

---
+++ Assets/UIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+	private GameObject gameoverText;
+
+	private GameObject runLenthText;
+
+	private float len = 0;
+
+	private float speed = 0.03f;
+
+	private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		this.gameoverText = GameObject.Find ("GameOver");
+		this.runLenthText = GameObject.Find ("RunLength");
+	}
+
+	// Update is called once per frame
+	void Update () {
+			if (this.isGameOver == false){
+				this.len += this.speed;
+
+				this.runLenthText.GetComponent<Text> ().text ="Distance: " + len.ToString ("F2") + "m";
+			}
+
+		if (this.isGameOver == true) {
+			if (Input.GetMouseButtonDown (0)) {
+				SceneManager.LoadScene ("GameScene");
+			}
+		}
+
+	}
+
+	public void GameOver(){
+		this.gameoverText.GetComponent<Text> ().text = "GameOver";
+		this.isGameOver = true;
+	}
+}
